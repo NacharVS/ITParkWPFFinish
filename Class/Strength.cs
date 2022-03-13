534c0c84d72ee34e57c2eaf70eaccfa1410a45f2
ProsvirninAV1 <AVProsvirnin@bk.ru>
2022-03-13 20:58:35 +0400

    fix

---
+++ Class/Strength.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCharacterEditor
+{
+    class Strength
+    {
+        public int Value { get; set; }
+
+        public int MinStrength { get; set; }
+
+        public int MaxStrength { get; set; }
+    }
+}
