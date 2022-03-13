0a15896e91df7dae91c70d0a3044905494d11e68
Petrikov Nikolay <petrikovnl@gmail.com>
2022-03-13 21:05:34 +0500

    Player

---
+++ 2022-03-13_VerificationWork/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_03_13_VerificationWork
+{
+    internal class Players
+    {
+    }
+}
