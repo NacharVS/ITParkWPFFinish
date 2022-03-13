c804f08e47e9aaeb7099c1793aa05a3fbef4d397
daryazakharova <daryazakharova87@gmail.com>
2022-03-13 20:02:57 +0300

    My project

---
+++ WpfApp2/ICharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public interface ICharacter
+    {
+        public double CurrentHealth { get; set; }
+        public double MaxHealth { get; set; }
+        public double MinHealth { get; set; }
+
+
+        void UnitInfo();
+
+
+    }
+}
