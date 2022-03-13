3a3e8e43e1c54969e8a3ad8ec34fdd4b8aa0bfe5
AndreiZasuhin <Bitolborg@yandex.ru>
2022-03-13 20:36:49 +0300

    1

---
+++ RPG/Personag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    internal interface Personag
+    {
+        static int strenght = 10;
+        static int agility = 10;
+        static int intelligence = 10;
+        static int endurance = 10;
+    }
+}
