0d6f8957ac6c06b3ec617646ad49f0325e2d4b06
ProsvirninAV1 <AVProsvirnin@bk.ru>
2022-03-14 22:23:01 +0400

    fix

---
+++ Class/MongoDBBase.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCharacterEditor.Class
+{
+    class MongoDBBase
+    {
+        public static void SetCharacter(string name, int strength, int agility, int intelligence, int endurance)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Сharacters");
+            var collection = database.GetCollection<Сharacter>("Сharacters");
+            collection.InsertOne(new Сharacter(name, strength, agility, intelligence, endurance));
+        }
+
+        public static List<string> GetListCharacters()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Сharacters");
+            var collection = database.GetCollection<Сharacter>("Сharacters");
+            var listCharactersFromDB = collection.Find(x => true).ToList();
+            List<string> listToReturn = new List<string>();
+            foreach (var item in listCharactersFromDB)
+            {
+                listToReturn.Add(item.Name);
+            }
+            return listToReturn;
+        }
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using WpfCharacterEditor.Class;
+
+namespace WpfCharacterEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string buffName = "Name";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void name_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void name_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (buffName != "Name")
+            {
+                return;
+            }
+            else
+                name.Clear();
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            MongoDBBase.SetCharacter(name.Text, int.Parse(strength.Content.ToString()), int.Parse(agility.Content.ToString()), int.Parse(intelligence.Content.ToString()), int.Parse(endurance.Content.ToString())); ;
+            listCharacter.ItemsSource = MongoDBBase.GetListCharacters();
+            MessageBox.Show($"Character {name.Text} has added.");
+        }
+
+        
+    }
+}
