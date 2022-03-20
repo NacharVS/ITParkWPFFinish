d426cb6e9e38ed12031301a1f30d271de495b59b
ProsvirninAV1 <AVProsvirnin@bk.ru>
2022-03-20 11:04:16 +0400

    fix

---
+++ Class/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCharacterEditor
+{
+    public class MyClass
+    {
+        public int Value { get; set; }
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+    }
+}
